c3ac328322935bc2ca9c1df92401aae359eda98d
Dmytro Borodin <dimabor96@gmail.com>
2020-10-28 17:03:19 +0200

    my last commit 28.10.2020

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/logo.png';
+import logo from './assets/logo.svg';
 import Feature from './Feature/FeatureComponent';
-import icon from './assets/icon_1.png';
+import icon_1 from './assets/icon_1.svg';
+import icon_2 from './assets/icon_2.svg';
+import icon_3 from './assets/icon_3.svg';
+import icon_4 from './assets/icon_4.svg';
 import Input from "./Input/InputComponent";
 import MainButton from "./MainButton/MainButtonComponent";
 
@@ -10,54 +13,50 @@ import MainButton from "./MainButton/MainButtonComponent";
 function App(props) {
   return (
     <div className="App">
-      <div className="container">
-          <div className="row">
-              <div className="text-block">
-                  <div className="title-block">
-                      <img src={logo} className="logo"/>
-                      <h3 className="page-title">
-                          Stay global. Stay Learn. Save 80%
+        <div className="container">
+              <div className="row">
+                  <div className="text-block">
+                      <div className="title-block">
+                          <img src={logo} className="logo"/>
+                          <h3 className="page-title">
+                              Stay global. Stay Learn. Save 80%
+                          </h3>
+                      </div>
+                      <p className="page-sub-title">
+                          International Payments for SME's
+                      </p>
+                      <div className="features-block">
+                          <Feature icon={icon_1} id='ic_1' title='Simple' text='Make it easier for international customers to pay you.'/>
+                          <Feature icon={icon_2} id='' title='Economical' text='Cut your international transfers by more than 80%.'/>
+                          <Feature icon={icon_3} id='' title='Transparent' text='Be kept in the loop about all your international transfers.'/>
+                          <Feature icon={icon_4} id='' title='Secure' text='We use secure methods we already trust.'/>
+                      </div>
+                  </div>
+                  <div className="form-block">
+                      <h3 className="form-title">
+                          Get started with Lean Paymants
                       </h3>
-                  </div>
-                  <p className="page-sub-title">
-                      International Payments for SME's
-                  </p>
-                  <div className="features-block">
-                    <Feature icon={icon} title='Simple' text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.'/>
-                    <Feature icon={icon} title='Simple' text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.'/>
-                    <Feature icon={icon} title='Simple' text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.'/>
-                    <Feature icon={icon} title='Simple' text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.'/>
-                  </div>
-              </div>
-              <div className="form-block">
-                  <h3 className="form-title">
-                      Get started with Lean Paymants
-                  </h3>
-                  <p className="form-subtitle">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing
-                  </p>
-                  <Input placeholder={'First Name'} />
-                  <Input placeholder={'Last Name'} />
-                  <Input placeholder={'Email'} />
-                  <Input placeholder={'Personal Cell'} />
-                  <MainButton btnName={'Create Account'}/>
-                  <div className="agreement-block">
-                      <input type='checkbox'/>
-                      <p className="main-text">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                      <p className="form-subtitle">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                      </p>
+                      <Input placeholder={'First Name'} />
+                      <Input placeholder={'Last Name'} />
+                      <Input placeholder={'Email'} />
+                      <Input placeholder={'Personal Cell'} />
+                      <MainButton btnName={'Create Account'}/>
+                      <div className="agreement-block">
+                          <input type='checkbox'/>
+                          <p className="main-text">
+                              By creating an account, you are agreeing to our terms.
+                          </p>
+                      </div>
+                      <p className="main-text flex">
+                          Already have an account?
+                          <a>Sign In</a>
                       </p>
                   </div>
-                  <p className="main-text flex">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
-                      <a>Sign In</a>
-                  </p>
               </div>
           </div>
-      </div>
     </div>
   );
 }
--- src/Feature/FeatureComponent.jsx
+++ src/Feature/FeatureComponent.jsx
@@ -5,7 +5,7 @@ const Feature = (props) => {
     return (
         <div className={f.feature_block}>
             <div className={f.title_block}>
-                <img src={props.icon} className={f.icon}/>
+                <img src={props.icon} className={f.icon} id={props.id}/>
                 <h4 className={f.title}>
                     {props.title}
                 </h4>
